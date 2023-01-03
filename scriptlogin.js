2811235b79a5e9fe854a24f549e9de234e2a3daf
sonuengineer <prajapatisonu50@gmail.com>
2023-01-03 14:57:34 +0530

    login fronted

---
+++ scriptlogin.js
@@ -0,0 +1,30 @@
+let email=document.querySelector('#emaillog');
+let pwd=document.querySelector('#pwdlog');
+let btnsubmit=document.querySelector('#btnlogin');
+
+
+btnsubmit.addEventListener('click',(e)=>{
+  e.preventDefault();
+
+  let email2=email.value;
+  let pwd2=pwd.value;
+
+  let obj={
+    email:email2,
+    pwd:pwd2
+  }
+
+  axios.post("http://localhost:5555/login", obj)
+  .then(result=>{
+    if(result.data.msg=='login successful'){
+      alert("signup successfully");
+      localStorage.setItem('token',result.data.token);
+     
+    }
+  })
+  .catch(err=>{
+  console.log(err);
+  })
+
+
+})
